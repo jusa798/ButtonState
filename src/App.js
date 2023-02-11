c76e5dd7734b0ed4cf8229ac0616decaeee30bd5
vinicius-gama <almeidagama97@gmail.com>
2023-02-10 18:24:18 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
   const [number, setNumber] = useState(0);
 
+  const[userInputNumber, setuserInput] = useState(0)
+  
   const handleIncrement = () => {
     setNumber((number) => number + 1);
   };
@@ -12,15 +14,33 @@ function App() {
     setNumber((number) => number - 1);
   };
 
+  const handleMultiply = () => {
+    setNumber((number) => number * 2 );
+  };
+  const handleDivide = () => {
+    setNumber((number) => number /2 )
+  };
+
+
+const setInput = () => {
+  const inNumber = parseInt(userInputNumber);
+  setNumber(inNumber);
+}
+ const getInput= (e) => {
+  setuserInput(e.target.value)
+ }
+
+
   return (
     <div className='app-container'>
       <div className='number'>{number}</div>
       <div>
         <button onClick={() => handleIncrement()}>Increment</button>
         <button onClick={() => handleDecrement()}>Decrement</button>
-        {/* MULTIPLY */}
-        {/* DIVIDE */}
-        {/* SQUARE */}
+        <button onClick={() => handleMultiply()}>Multiply</button>
+        <button onClick={() => handleDivide()}>Divide</button>
+        <input onChange={(e)=> getInput(e)}></input>
+        <button onClick ={()=> setInput()}>SUBMIT</button>
       </div>
     </div>
   );
